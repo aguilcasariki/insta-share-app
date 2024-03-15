523b8124482d11314e69385afe266c0ec6922af2
Riki <aguilcasariki@gmail.com>
2024-03-15 18:38:27 -0400

    Use fetchServerData in UserWorkspace page

--- app/(client)/workspace/[user]/page.js
+++ app/(client)/workspace/[user]/page.js
@@ -1,22 +1,13 @@
 import React from "react";
 import UserWorkspace from "./components/UserWorkspace/UserWorkspace";
+
+import fetchServerData from "../../services/fetchServerData";
 import { TableProvider } from "./context/TableContext";
 
-async function getData(params) {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_FILES_API_URL}/${params.user}/files`,
-    { cache: "no-cache" }
-  );
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
-}
-
 const UserWorkspacePage = async ({ params }) => {
-  const filesData = await getData(params);
+  const filesData = await fetchServerData(
+    `${process.env.NEXT_PUBLIC_FILES_API_URL}/${params.user}/files`
+  );
   return (
     <TableProvider>
       <UserWorkspace user={params.user} filesData={filesData} />
